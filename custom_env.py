323c88a14adfc963276d72738f6ef776aec38518
jkrasz <john.kraszewski@gmail.com>
2023-09-24 15:32:29 -0700

    A meaningful commit message describing the changes you made

--- custom_env.py
+++ custom_env.py
@@ -1,83 +1,53 @@
-import numpy as np
 import gym
-from gym import spaces
+import numpy as np
+import pandas as pd
 
-class CustomTradingEnvironment(gym.Env):
-
-    def __init__(self, data, features, targets):
-        super(CustomTradingEnvironment, self).__init__()
-
+class StockTradingEnv(gym.Env):
+    def __init__(self, data):
+        super(StockTradingEnv, self).__init__()
         self.data = data
-        self.features = features
-        self.targets = targets
         self.current_step = 0
-        self.current_position = None  # 0: No position, 1: Long, 2: Short
-        self.current_price = self._get_current_price()
+        self.action_space = gym.spaces.Discrete(4)  # For 4 discrete actions
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(len(data.columns),))  # Modify according to your data
+        self.holding = False  # To keep track of whether we are holding a stock or not
+        self.buy_price = 0  # To store the price at which we bought the stock
 
-        self.action_space = spaces.Discrete(4)  # 0: Hold, 1: Buy (Go long), 2: Sell (Go short), 3: Close position
-
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3*self.features.shape[1] + 2,), dtype=np.float32)
+    def step(self, action):
+        if self.current_step + 1 >= len(self.data):
+            done = True
+            reward = self._calculate_final_reward()
+            obs = self.data.iloc[self.current_step]  # Return as a Series instead of array
+        else:
+            self.current_step += 1
+            done = False
+            reward = self._calculate_reward(action, self.data.iloc[self.current_step])
+            obs = self.data.iloc[self.current_step]  # Return as a Series instead of array
+        return obs, reward, done, {}
 
     def reset(self):
         self.current_step = 0
-        self.current_position = 0
+        return self.data.iloc[self.current_step].values
 
-        obs = self.features.iloc[self.current_step].values
-        sma_5 = np.zeros(self.features.shape[1])
-        sma_10 = np.zeros(self.features.shape[1])
+    def _calculate_reward(self, action, obs):
+        close_price = obs['Close']
+        if action == 1:  # Buying
+            if not self.holding:  # Can buy only if not already holding
+                self.holding = True
+                self.buy_price = close_price
+                return 1  # Encouragement Reward for Buying
+            else:
+                return -1  # Penalty for invalid action
+        elif action == 2:  # Selling
+            if self.holding:  # Can sell only if currently holding
+                self.holding = False
+                profit = close_price - self.buy_price
+                return profit  # Profit (could be negative) as Reward
+            else:
+                return -1  # Penalty for invalid action
+        return 0  # Neutral reward for holding or other actions
 
-        self.state = np.concatenate([obs, sma_5, sma_10, [self.current_position], [self.current_price]])
-
-        return self.state.astype(np.float32)
-
-    def _get_current_price(self):
-        if 'Close' in self.data.columns:
-            return self.data['Close'].iloc[-1]
-        else:
-            raise ValueError("The data does not have a recognized close price column.")
-
-    def step(self, action):
-        self.current_step += 1
-        self.current_price = self._get_current_price()
-
-        obs = self.features.iloc[self.current_step].values
-
-        if self.current_step >= 5:
-            sma_5 = self.features.iloc[self.current_step-5:self.current_step].mean().values
-        else:
-            sma_5 = np.zeros(self.features.shape[1])
-
-        if self.current_step >= 10:
-            sma_10 = self.features.iloc[self.current_step-10:self.current_step].mean().values
-        else:
-            sma_10 = np.zeros(self.features.shape[1])
-
-        obs = np.concatenate([obs, sma_5, sma_10, [self.current_position], [self.current_price]]).astype(np.float32)
-
-        reward = self.calculate_reward(action)
-        done = self.current_step >= len(self.features) - 1
-
-        if action == 1:
-            self.current_position = 1
-        elif action == 2:
-            self.current_position = 2
-        elif action == 3:
-            self.current_position = 0
-
-        return obs, reward, done, {}
-
-    def calculate_reward(self, action):
-        if self.current_position == 0:
-            return 0.0
-
-        current_price = self.targets[self.current_step]
-        next_price = self.targets[self.current_step + 1] if self.current_step < len(self.targets) - 1 else current_price
-
-        if self.current_position == 1:  # Long
-            reward = next_price - current_price
-        elif self.current_position == 2:  # Short
-            reward = current_price - next_price
-        else:
-            reward = 0.0
-
-        return float(reward)
+    def _calculate_final_reward(self):
+        if self.holding:  # Still holding a stock at the end
+            final_profit = self.data.iloc[-1]['Close'] - self.buy_price
+            return final_profit  # Could be negative
+        return 0  # No profit or loss
--- trade_1.py
+++ trade_1.py
@@ -1,188 +1,56 @@
-import logging, json, smtplib, pytz, warnings
-from pathlib import Path
-import numpy as np
 import pandas as pd
-from ta.momentum import RSIIndicator
-from ta.volatility import BollingerBands
-from ta.trend import EMAIndicator
-from datetime import datetime, time
-from time import sleep
-from sklearn.exceptions import DataConversionWarning
-from stable_baselines3 import DQN, PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.env_checker import check_env
-from custom_env import CustomTradingEnvironment
+import numpy as np
 import yfinance as yf
-from dateutil.relativedelta import relativedelta
+from stable_baselines3 import PPO
+from custom_env import StockTradingEnv
+from stable_baselines3.common.vec_env import DummyVecEnv
+import smtplib
+import ta  # For additional technical indicators
+from datetime import datetime, timedelta
+import pytz
 
-warnings.filterwarnings(action='ignore', category=DataConversionWarning)
-log_file = Path("trade_log.json")
+# Your existing functions and definitions...
 
-if log_file.exists():
-    with open(log_file, "r") as file:
-        log_data = json.load(file)
-    position = log_data["position"]
-    entry_price = log_data["entry_price"]
-    max_price = log_data["max_price"]
-    min_price = log_data["min_price"]
-else:
-    log_data = {"position": 0, "entry_price": 0, "max_price": 0, "min_price": 0}
-
-logging.basicConfig(filename="algorithm.log", level=logging.INFO)
-
-def make_decision(model_prediction, rsi_value, ema20, ema50):
-    if model_prediction == 1 and rsi_value < 30 and ema20 > ema50: # Over-sold and uptrend
-        return 1
-    elif model_prediction == 2 and rsi_value > 70 and ema20 < ema50: # Over-bought and downtrend
-        return 2
-    elif model_prediction == 3:
-        return 3
-    else:
-        return
-
-def train_model(env, model_save_path='ppo_model.pkl'):
-    model = PPO('MlpPolicy', env, verbose=1, n_steps=2048, ent_coef=0.005, learning_rate=0.00025, vf_coef=0.5, max_grad_norm=0.5, gae_lambda=0.95, n_epochs=4, clip_range=0.2, clip_range_vf=None)
-    model.learn(total_timesteps=100000)
-    model.save(model_save_path)
-    return model
-
-def load_model(model_save_path='ppo_model.pkl'):
-    model = PPO.load(model_save_path)
-    return model
-def prepare_data(data):
-    data = data.dropna()
-    features = data.drop('Close', axis=1)
-    targets = data['Close'].shift(-1).dropna()
-    features = features[:-1]
-    return features, targets
-
-
-def send_email(subject, message_body):
-    sender_email = 'chatGptTrade@gmail.com'
-    sender_password = 'bymwlvzzmbzxeeas'
-    receiver_email = 'john.kraszewski@gmail.com'
-    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp_server.starttls()
-    smtp_server.login(sender_email, sender_password)
-    message = f"Subject: {subject}\n\n{message_body}"
-    print(message)
-    try:
-        smtp_server.sendmail(sender_email, receiver_email, message)
-    except Exception as e:
-        print("Error sending email: ", e)
-    smtp_server.quit()
-
-def is_market_open():
-    opening_time = time(9, 30)
-    closing_time = time(16, 0)
-    now = datetime.now(pytz.timezone('US/Eastern'))
-    return opening_time <= now.time() <= closing_time and now.weekday() < 5
-
+# Fetch the data
 symbol = 'GPRO'
-model_save_path = 'ppo_model.pkl'
+end_date = datetime.now() - timedelta(days=1)
+data = yf.download(symbol, start="2020-01-01", end=end_date.strftime('%Y-%m-%d'))
 
-data = yf.download(symbol, period="5y")
-data = data.sort_index(ascending=True)
-print(data)
+# Additional Technical Indicators
+data['SMA_5'] = data['Close'].rolling(window=5).mean()
+data['SMA_20'] = data['Close'].rolling(window=20).mean()
+data['MACD'] = data['Close'].ewm(span=12, adjust=False).mean() - data['Close'].ewm(span=26, adjust=False).mean()
+data['RSI'] = ta.momentum.rsi(data['Close'])
+data['ATR'] = ta.volatility.average_true_range(data['High'], data['Low'], data['Close'])
 
-features, targets = prepare_data(data)
-env = CustomTradingEnvironment(data, features, targets)
-check_env(env)
-vec_env = DummyVecEnv([lambda: env])
+data.dropna(inplace=True)
 
-today = datetime.today()
-start_date = today - relativedelta(months=6)
-end_date = today
-data = yf.download(symbol, start=start_date, end=end_date)
-features, targets = prepare_data(data)
-env = CustomTradingEnvironment(data, features, targets)
-vec_env = DummyVecEnv([lambda: env])
-train = True
-if train:
-    model = train_model(vec_env, model_save_path=model_save_path)
-else:
-    model = load_model(model_save_path=model_save_path)
+# Define the environment and model
+env = DummyVecEnv([lambda: StockTradingEnv(data)])
+model = PPO("MlpPolicy", env, verbose=1)
 
-
-model_save_interval = 1000
-position = 0
-max_price = 0
-min_price = 0
-entry_price = 0
-last_action = None
 while True:
-    if True:
     #if is_market_open():
-        print('in true')
+    if True:
+        # Train the model
+        model.learn(total_timesteps=20000)
+
+        # Simulated Trading
         obs = env.reset()
-        data = yf.download(symbol, period="1d", interval="15m")
-        ema_indicator_20 = EMAIndicator(data['Close'], window=20)
-        ema_indicator_50 = EMAIndicator(data['Close'], window=50)
-        data['EMA20'] = ema_indicator_20.ema_indicator()
-        data['EMA50'] = ema_indicator_50.ema_indicator()
-        rsi_indicator = RSIIndicator(data['Close'])
-        data['RSI'] = rsi_indicator.rsi()
-        # Getting current RSI, EMA20, EMA50
-        current_rsi = data['RSI'].iloc[-1]
-        current_ema20 = data['EMA20'].iloc[-1]
-        current_ema50 = data['EMA50'].iloc[-1]
-        model_prediction, _ = model.predict(obs, deterministic=True)
-        # Now we take the action based on our new decision-making function
-        action = make_decision(model_prediction, current_rsi, current_ema20, current_ema50)
+        for i in range(len(data)):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, rewards, done, info = env.step(action)
 
+            if action == 1:
+                print(f"Buying at step {i}, price: {obs['Close']}")
+                send_email("Buy Signal", f"Buying at step {i}, price: {obs['Close']}")
+            elif action == 2:
+                print(f"Selling at step {i}, price: {obs['Close']}")
+                send_email("Sell Signal", f"Selling at step {i}, price: {obs['Close']}")
 
-        if action is not None:
-            obs, reward, done, info = env.step(action)
-        else:
-            # Handle the case where action is None.
-            # For example, you can skip the step and log a warning.
-            logging.warning("Action is None, skipping this step.")
-            #continue
-
-
-        if action != last_action:
-            last_action = action
-            if action == 1:  # Buy
-                position = 1
-                entry_price = env.current_price
-                max_price = env.current_price
-                min_price = np.inf
-                send_email("Entering LONG position", f"Entry price: {entry_price}")
-                logging.info(f"{datetime.now(pytz.timezone('US/Eastern'))} - Entering LONG position at {entry_price}")
-
-            elif action == 2:  # Sell
-                position = -1
-                entry_price = env.current_price
-                max_price = -np.inf
-                min_price = env.current_price
-                send_email("Entering SHORT position", f"Entry price: {entry_price}")
-                logging.info(f"{datetime.now(pytz.timezone('US/Eastern'))} - Entering SHORT position at {entry_price}")
-
-            elif action == 3:  # Close position
-                if position == 1:  # Close long position
-                    pnl = env.current_price / entry_price - 1
-                    send_email("Closing LONG position", f"Close price: {env.current_price}, PnL: {pnl:.2%}")
-                    logging.info(f"{datetime.now(pytz.timezone('US/Eastern'))} - Closing LONG position at {env.current_price}, PnL: {pnl:.2%}")
-
-                elif position == -1:  # Close short position
-                    pnl = entry_price / env.current_price - 1
-                    send_email("Closing SHORT position", f"Close price: {env.current_price}, PnL: {pnl:.2%}")
-                    logging.info(f"{datetime.now(pytz.timezone('US/Eastern'))} - Closing SHORT position at {env.current_price}, PnL: {pnl:.2%}")
-
-        log_data["position"] = position
-        log_data["entry_price"] = entry_price
-        log_data["max_price"] = max_price
-        log_data["min_price"] = min_price
-
-        with open(log_file, "w") as file:
-            json.dump(log_data, file)
-
-        if env.current_step % model_save_interval == 0:
-            model.save(model_save_path)
-            print(f"Model saved at step {env.current_step}")
-
+            if done:
+                obs = env.reset()
+        print("Simulation Completed!")
     else:
-        print('market is closed')
-        sleep(60 * 60)  # Sleep for an hour
-
-    sleep(60 * 15)  # Sleep for 15 minutes
+        print("Market is closed. Sleeping...")
+        sleep(60 * 15)  # Sleep for 15 minutes before checking again
