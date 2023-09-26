1419fcebb517c72cf2357abb5841b6cdf882da5d
jkrasz <john.kraszewski@gmail.com>
2023-09-25 22:13:52 -0700

    A meaningful commit message describing the changes you made

--- trade_1.py
+++ trade_1.py
@@ -6,10 +6,32 @@ from custom_env import StockTradingEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
 import smtplib
 import ta  # For additional technical indicators
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 import pytz
+from time import sleep
 
-# Your existing functions and definitions...
+
+def is_market_open():
+    opening_time = time(9, 30)
+    closing_time = time(16, 0)
+    now = datetime.now(pytz.timezone('US/Eastern'))
+    return opening_time <= now.time() <= closing_time and now.weekday() < 5
+
+def send_email(subject, content):
+    sender_email = 'chatGptTrade@gmail.com'
+    sender_password = 'bymwlvzzmbzxeeas'
+    receiver_email = 'john.kraszewski@gmail.com'
+    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_server.starttls()
+    smtp_server.login(sender_email, sender_password)
+    message = f"Subject: {subject}\n\n{content}"
+    print(message)
+    try:
+        smtp_server.sendmail(sender_email, receiver_email, message)
+    except Exception as e:
+        print("Error sending email: ", e)
+    smtp_server.quit()
+
 
 # Fetch the data
 symbol = 'GPRO'
@@ -30,8 +52,8 @@ env = DummyVecEnv([lambda: StockTradingEnv(data)])
 model = PPO("MlpPolicy", env, verbose=1)
 
 while True:
-    #if is_market_open():
-    if True:
+    if is_market_open():
+    #if True:
         # Train the model
         model.learn(total_timesteps=20000)
 
